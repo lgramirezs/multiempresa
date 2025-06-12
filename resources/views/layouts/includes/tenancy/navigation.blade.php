e3fdc37638b374246174ba156d710d3970abc965
Luis Ramírez <ramirezluis1598@gmail.com>
2025-06-12 18:27:28 -0400

    Ajustes en el controllador

--- resources/views/layouts/includes/tenancy/navigation.blade.php
+++ resources/views/layouts/includes/tenancy/navigation.blade.php
@@ -15,9 +15,6 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('tenants.index')" :active="request()->routeIs('tenants.index')">
-                        {{ __('Tenants') }}
-                    </x-nav-link>
                 </div>
             </div>
 
